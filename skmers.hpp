cb6235727c1767d2a4a980cad7a9272017d20d73
Yoann Dufresne <yoann.dufresne0@gmail.com>
2023-11-10 18:12:22 +0100

    basic hash function

---
+++ skmers.hpp
@@ -0,0 +1,19 @@
+#include <string>
+
+#include "hash.hpp"
+
+
+class SkmerStreamer
+{
+public:
+	uint64_t minimizer_size;
+	uint64_t current_position;
+	Hash h;
+	std::string & sequence;
+
+	SkmerStreamer(std::string & sequence, uint64_t m, Hash h)
+										: minimizer_size(m)
+										, current_position(0)
+										, sequence(sequence)
+	Skmer next();	
+}
