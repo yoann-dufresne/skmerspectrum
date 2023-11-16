a769ccb65760543c534d3ea25b6e77283afa4348
yoann-dufresne <yoann.dufresne0@gmail.com>
2023-11-16 16:41:17 +0100

    hash templating

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+#include "hash.hpp"
+
+
+char * test_seq = "ACCTGTAAGTACCA";
+uint64_t seq_size = 14;
+
+
 int main(int argc, char const *argv[])
 {
 	std::cout << "Hello world" << std::endl;
--- include/skmers.hpp
+++ include/skmers.hpp
@@ -3,17 +3,19 @@
 #include "hash.hpp"
 
 
+template<class H>
 class SkmerStreamer
 {
 public:
 	uint64_t minimizer_size;
 	uint64_t current_position;
-	Hash h;
+	H h;
 	std::string & sequence;
 
-	SkmerStreamer(std::string & sequence, uint64_t m, Hash h)
+	SkmerStreamer(std::string & sequence, uint64_t k, uint64_t m)
 										: minimizer_size(m)
 										, current_position(0)
+										, h(k)
 										, sequence(sequence)
 	{}
 
